0ce85634a69b59d859b2b44a9d873b1dce366fa3
rajan2023 <rajan.shah2022@gmail.com>
2021-10-25 15:29:46 +0545

    First repo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+import './App.scss';
+import {BrowserRouter,Route,Switch} from "react-router-dom";
+import AdminPanel from './pages/admin/login';
 const App =()=> {
   return (
     <div className="App">
-    
+      <BrowserRouter>
+      <Switch>
+    <Route path="/" exact component={AdminPanel} />
+    </Switch>
+    </BrowserRouter>
     </div>
   );
 }
